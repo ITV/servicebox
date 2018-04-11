99c07f3412d25351c16320331c70a858107f0100
Andrea Fiore <andrea.fiore@itv.com>
2018-04-11 11:27:27 +0100

    try force publish

--- ci/publish.sh
+++ ci/publish.sh
@@ -11,7 +11,7 @@ echo "version = $version"
 
 sbt_cmd="sbt ++$TRAVIS_SCALA_VERSION"
 
-if [[ $TRAVIS_TAG =~ ^v[0-9].* || ($TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master") ]]; then
+#if [[ $TRAVIS_TAG =~ ^v[0-9].* || ($TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master") ]]; then
   openssl aes-256-cbc -K $encrypted_1426a13332e0_key -iv $encrypted_1426a13332e0_iv -in ci/private.asc.enc -out ci/private.asc -d
   eval "$sbt_cmd clean +publishSigned"
-fi;
+#fi;
